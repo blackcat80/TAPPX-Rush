ca5a771ee35c67e93e2267f71b89d939024e44f1
blackcat80 <csitja80@gmail.com>
2023-03-18 23:19:34 +0100

    limpieza de texto de articulos

--- pruebas.py
+++ pruebas.py
@@ -1,10 +1,18 @@
+from base64 import encode
+from cgitb import text
+from lib2to3.pgen2 import tokenize
+from pyexpat import model
 import nltk
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 from string import punctuation
+from transformers import BertTokenizer, BertModel
+import torch
 import matplotlib.pyplot as plt
 import json
+
+model = BertModel.from_pretrained('bert-base-uncased')
 
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
@@ -19,12 +27,43 @@ tokenizer = RegexpTokenizer(patron)
 
 for id, dictt in data.items():
     words = tokenizer.tokenize(data[id]["text"].lower())
-    filtered_tokens = [token for token in words if token not in stopwords_es]
+    filtered_tokens = [token for token in words if token not in stopwords_es and len(token) > 2 and (token) not in punctuation]
     #for index, word in enumerate(filtered_tokens):
      #   filtered_tokens[index] = " ".join(filtered_tokens[index])
     print(filtered_tokens)
     print("\n"*3)
 
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+tokenized_text = tokenizer.tokenize(data[id]["text"].lower())
+
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+tokens_tensor = torch.tensor([indexed_tokens])
+
+with torch.no_grad():
+    encoded_layers, _ = model(tokens_tensor)
+
+# Calcular la puntuación de cada token
+layer_i = 0
+batch_i = 0
+token_i = 0
+
+# Obtener el vector de características del token
+token_embeddings = torch.stack(encoded_layers[layer_i][batch_i][token_i:])
+
+# Sumar las capas para obtener la representación de la palabra
+summed_token_embeddings = torch.sum(token_embeddings, dim=0)
+
+# Convertir el tensor en una matriz de numpy para facilitar la manipulación
+summed_token_embeddings = summed_token_embeddings.numpy()
+
+# Calcular la puntuación de cada palabra
+word_scores = {}
+for i, word in enumerate(text):
+    word_scores[word] = summed_token_embeddings[i]
+
+
+print(indexed_tokens)
 #data[id]["text"] = filtered_tokens
 #freq_dist = nltk.FreqDist(words)
 
